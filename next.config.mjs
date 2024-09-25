6b7511214e18ae1a71940e8a1d0834a16b73fa4f
liuhanyan <hanyen0828@gmail.com>
2024-09-25 22:16:06 +0800

    resolve typeerror

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config) => {
+        // Disable the canvas module
+        config.resolve.alias['canvas'] = false;
+        return config;
+    },
+};
 
 export default nextConfig;
