98491dd082c4b0afeaac99a2371cd978e5dfccc0
liuhanyan <hanyen0828@gmail.com>
2024-09-25 22:59:29 +0800

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
     webpack: (config) => {
         // Disable the canvas module
         config.resolve.alias['canvas'] = false;
